364bb9a5a41a7dd7001ffb2b8b453d48961a1b3e
Willie Boonzaier <126150284+Williamwhy@users.noreply.github.com>
2025-06-03 10:28:32 +0200

    Update nnUNetTrainer.py

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -178,7 +178,7 @@ class nnUNetTrainer(object):
         
         wandb.login(key="f0ca71ae6912c78891567a52c333cd3f61a8749a")
         run = wandb.init(
-            project="nnUnetQR",  # Specify your project
+            project="nnUnetHR",  # Specify your project
                   # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
             name = f"nnUNet_{int(time())}",
             config={                        # Track hyperparameters and metadata
