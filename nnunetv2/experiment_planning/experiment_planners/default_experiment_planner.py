e40c3bba2cd83c0a9cf998137705d22cc2bb93db
Willie Boonzaier <126150284+Williamwhy@users.noreply.github.com>
2025-06-10 11:42:38 +0200

    Update default_experiment_planner.py

--- nnunetv2/experiment_planning/experiment_planners/default_experiment_planner.py
+++ nnunetv2/experiment_planning/experiment_planners/default_experiment_planner.py
@@ -472,7 +472,7 @@ class ExperimentPlanner(object):
                 print(f'Dropping 3d_lowres config because the image size difference to 3d_fullres is too small. '
                       f'3d_fullres: {new_median_shape_transposed}, '
                       f'3d_lowres: {[round(i) for i in plan_3d_fullres["spacing"] / lowres_spacing * new_median_shape_transposed]}')
-                #plan_3d_lowres = None
+                plan_3d_lowres = None
             if plan_3d_lowres is not None:
                 plan_3d_lowres['batch_dice'] = False
                 plan_3d_fullres['batch_dice'] = True
